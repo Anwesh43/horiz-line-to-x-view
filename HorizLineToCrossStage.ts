0525cd3238004bfcf640cca7851ee23ad7751d35
Anwesh43 <anweshthecool0@gmail.com>
2019-06-23 11:17:42 +0530

    adding HorizLineToCrossStage class

--- HorizLineToCrossStage.ts
+++ HorizLineToCrossStage.ts
@@ -68,3 +68,34 @@ class DrawingUtil {
         context.restore()
     }
 }
+
+class HorizLineToCrossStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : HorizLineToCrossStage = new HorizLineToCrossStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
