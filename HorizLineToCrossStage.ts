e434c63c1393cf7d3d5539a838560c54cdca86dc
Anwesh43 <anweshthecool0@gmail.com>
2019-06-23 11:05:55 +0530

    adding DrawingUtil class

--- HorizLineToCrossStage.ts
+++ HorizLineToCrossStage.ts
@@ -32,3 +32,39 @@ class ScaleUtil {
         return ScaleUtil.mirrorValue(scale, a, b) * dir
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawHorizLineToPlus(context : CanvasRenderingContext2D, i : number, scale : number, size : number) {
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(Math.PI / 2 * ScaleUtil.divideScale(scale, i, lines) * j)
+            DrawingUtil.drawLine(context, 0, 0, size * (1 - 2 * i), 0)
+            context.restore()
+        }
+    }
+
+    static drawHLTCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / sizeFactor
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, 2)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, 2)
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.save()
+        context.translate(gap * (i + 1), h / 2)
+        context.rotate((Math.PI / 4) * sc2)
+        for (var j = 0; j < lines; j++) {
+            DrawingUtil.drawHorizLineToPlus(context, j, sc1, size)
+        }
+        context.restore()
+    }
+}
