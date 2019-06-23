5544ef3d356ecdddb78031f3d66a2f50cf9ae221
Anwesh43 <anweshthecool0@gmail.com>
2019-06-23 11:36:56 +0530

    adding HLTCNode class

--- HorizLineToCrossStage.ts
+++ HorizLineToCrossStage.ts
@@ -143,3 +143,48 @@ class Animator {
         }
     }
 }
+
+class HLTCNode {
+
+    next : HLTCNode
+    prev : HLTCNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new HLTCNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawHLTCNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : HLTCNode {
+         var curr : HLTCNode = this.prev
+         if (dir == 1) {
+            curr = this.next
+         }
+         if (curr) {
+            return curr
+         }
+         cb()
+         return this
+    }
+}
